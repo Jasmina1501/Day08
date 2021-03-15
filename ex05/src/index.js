dc0c8e93b19bf871ff0d40fdf4af69ed0f425dfd
Jasmina Secic <jasmina.secic98@outlook.com>
2021-03-15 22:41:55 +0100

    Functions - Global vs. Local Scope

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+var dress = "little dress";
+
+function myDress() {
+    var dress = "ball gown";
+    console.log(dress);
+    return dress;
+}
+myDress();
+
+console.log(dress);
+
+module.exports = myDress;
