d258f7559fd7a44568f7db5c772cfece77d935bf
Jasmina Secic <jasmina.secic98@outlook.com>
2021-03-16 00:43:34 +0100

    the parseInt()

---
+++ ex14/src/index.js
@@ -0,0 +1,9 @@
+function convertStrToInt(str) {
+    return parseInt(str, 10);
+}
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
+
+module.exports=convertStrToInt;
