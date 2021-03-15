f11c676df99e9ef6143794522cc1f70e2d281ffa
Jasmina Secic <jasmina.secic98@outlook.com>
2021-03-15 11:11:20 +0100

    Function with Arguments

---
+++ ex02/src/index.js
@@ -0,0 +1,6 @@
+function functionPrintInfo(parametar1, parametar2) {
+    console.log(parametar1, parametar2);
+}
+functionPrintInfo("John", "23");
+functionPrintInfo("Michael", "43");
+module.exports=functionPrintInfo;
